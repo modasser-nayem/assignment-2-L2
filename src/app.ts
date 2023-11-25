fa90e5d75b385fff8507062d5466225d7fa33103
modasser-nayem <modassernayem@gmail.com>
2023-11-25 11:32:44 +0600

    handle worng route request

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,17 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome Back Assignment-2');
 });
 
+app.use('*', (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found!',
+    error: {
+      code: 404,
+      description: `The ${req.url} not a valid URL!`,
+    },
+  });
+});
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-unused-vars, @typescript-eslint/no-unused-vars
 app.use(((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({
@@ -34,7 +45,6 @@ app.use(((err: any, req: Request, res: Response, next: NextFunction) => {
       description: err.message,
     },
   });
-  next(err);
 }) as ErrorRequestHandler);
 
 export default app;
